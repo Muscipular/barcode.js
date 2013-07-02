cbe67777adacdcdee187d25f44bc86dcb07c7033
Muscipular <admin@muscipular.net>
2013-07-02 14:41:25 +0800

    重构代码

--- code39/CommonHelper.js
+++ code39/CommonHelper.js
@@ -1,41 +1,28 @@
 var CommonHelper = {
-	CreateClass: function(fields, prototype, static) {
-		var getFunc = function() {
-				return function() {
-					if(fields instanceof Array) {
-						for(var field in fields) {
-							this[fields[field]] = null;
-						}
-					} else {
-						for(var field in fields) {
-							this[field] = fields[field];
-						}
-					}
-					this.ctor.apply(this, arguments);
-				};
-			}
-		var Class = getFunc();
-		prototype = prototype ? prototype : {};
-		if(!prototype.ctor) {
-			prototype.ctor = function() {};
+	Extend: function(target, source) {
+		function isBoolean(b) {
+			return typeof b == 'boolean' || b instanceof Boolean;
 		}
-		if(static) {
-			for(var field in static) {
-				Class[field] = static[field];
-			}
-		}
-		Class.prototype = prototype;
-		return Class;
-	},
-	extend: function(target, source, overWrite) {
-		if(undefined == overWrite) {
+		var overWrite, i, m;
+		if (arguments.length == 2) {
 			overWrite = true;
+		} else if (arguments.length > 2) {
+			overWrite = ((!isBoolean(overWrite = arguments[arguments.length - 1])) || (overWrite));
+		} else {
+			return target;
 		}
-		for(var field in source) {
-			if(overWrite || !(field in target)) {
-				target[field] = source[field];
+		m = arguments.length - (overWrite && isBoolean(arguments[arguments.length - 1]) ? 2 : 1);
+		for (i = 1; i <= m; i++) {
+			source = arguments[i];
+			if (i == m && m > 2 && isBoolean(source)) {
+				break;
+			}
+			for (var field in source) {
+				if (overWrite || !(field in target)) {
+					target[field] = source[field];
+				}
 			}
 		}
 		return target;
 	},
-}
+};
--- code39/draw39.js
+++ code39/draw39.js
@@ -1,75 +1,90 @@
-window.addEventListener("load", function() {
-	var c1 = document.getElementById('c1');
-	var panel = c1.getContext('2d');
+(function(w) {
+	var _config = {
+		Black: '#000',
+		White: '#fff',
+		Weight: 2,
+		Rate: 2.5,
+		Height: 200,
+		Panel: null,
+	};
 
-	var Code39List = [
-		new Code39('0', '000110100'),
-		new Code39('1', '100100001'),
-		new Code39('2', '001100001'),
-		new Code39('3', '101100000'),
-		new Code39('4', '000110001'),
-		new Code39('5', '100110000'),
-		new Code39('6', '001110000'),
-		new Code39('7', '000100101'),
-		new Code39('8', '100100100'),
-		new Code39('9', '001100100'),
-		new Code39('A', '100001001'),
-		new Code39('B', '001001001'),
-		new Code39('C', '101001000'),
-		new Code39('D', '000011001'),
-		new Code39('E', '100011000'),
-		new Code39('F', '001011000'),
-		new Code39('G', '000001101'),
-		new Code39('H', '100001100'),
-		new Code39('I', '001001101'),
-		new Code39('J', '000011100'),
-		new Code39('K', '100000011'),
-		new Code39('L', '001000011'),
-		new Code39('M', '101000010'),
-		new Code39('N', '000010011'),
-		new Code39('O', '100010010'),
-		new Code39('P', '001010010'),
-		new Code39('Q', '000000111'),
-		new Code39('R', '100000110'),
-		new Code39('S', '001000110'),
-		new Code39('T', '000010110'),
-		new Code39('U', '110000001'),
-		new Code39('V', '011000001'),
-		new Code39('W', '111000000'),
-		new Code39('X', '010010001'),
-		new Code39('Y', '110010000'),
-		new Code39('Z', '011010000'),
-		new Code39('-', '010000101'),
-		new Code39('%', '000101010'),
-		new Code39('$', '010101000'),
-		new Code39('*', '010010100'),
-		];
+	var Code39List = {
+		'0': '000110100',
+		'1': '100100001',
+		'2': '001100001',
+		'3': '101100000',
+		'4': '000110001',
+		'5': '100110000',
+		'6': '001110000',
+		'7': '000100101',
+		'8': '100100100',
+		'9': '001100100',
+		'A': '100001001',
+		'B': '001001001',
+		'C': '101001000',
+		'D': '000011001',
+		'E': '100011000',
+		'F': '001011000',
+		'G': '000001101',
+		'H': '100001100',
+		'I': '001001101',
+		'J': '000011100',
+		'K': '100000011',
+		'L': '001000011',
+		'M': '101000010',
+		'N': '000010011',
+		'O': '100010010',
+		'P': '001010010',
+		'Q': '000000111',
+		'R': '100000110',
+		'S': '001000110',
+		'T': '000010110',
+		'U': '110000001',
+		'V': '011000001',
+		'W': '111000000',
+		'X': '010010001',
+		'Y': '110010000',
+		'Z': '011010000',
+		'-': '010000101',
+		'%': '000101010',
+		'$': '010101000',
+		'*': '010010100'
+	};
 
-	window.Code39Genner = {
-		GenCode: function(s) {
+	w.Code39Genner = {
+		GenCode: function(str, config) {
+			config = CommonHelper.Extend({}, _config, config);
 			this.Clear();
-			if(!s) {
+			if (!str) {
 				return;
 			}
-			s = ["*", s.toUpperCase(), "*"].join('');
+			str = ["*", str.toUpperCase(), "*"].join('');
 			var line = 0;
-			for(var i = 0, max = s.length; i < max; i++) {
-				var ch = s[i];
-				for(var x = Code39List.length - 1; x >= 0; x--) {
-					var code39 = Code39List[x];
-					if(code39.char == ch) {
-						line = code39.draw(panel, line);
-						break;
-					}
-					if(x == 0) {
-						this.Clear();
-						return;
-					}
+			for (var i = 0, max = str.length; i < max; i++) {
+				var ch = str[i];
+				var code = Code39List[ch];
+				if (!code) {
+					throw "'" + ch + "' is invalid";
+				}
+				for (var j = 0; j < 9; j++) {
+					var width = code[j] & 1 ? config.Rate * config.Weight : config.Weight;
+					config.Panel.fillStyle = j & 1 ? config.White : config.Black;
+					config.Panel.fillRect(line, 0, width, config.Height);
+					line += width;
 				};
+				config.Panel.fillStyle = config.White;
+				config.Panel.fillRect(line, 0, config.Weight, config.Height);
+				line += config.Weight;
 			}
 		},
 		Clear: function() {
-			panel.clearRect(0, 0, 800, 600);
+			_config.Panel.clearRect(0, 0, 80000, 60000);
 		},
+		Init: function(id) {
+			var c1 = document.getElementById(id);
+			if (c1 && c1.getContext) {
+				_config.Panel = c1.getContext('2d');
+			}
+		}
 	}
-}, false);
+})(window);
